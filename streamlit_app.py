8bf4e818a97853dd183e13808ad8ca9de4dd2d8f
jy636 <jyl2003@med.cornell.edu>
2025-12-31 13:58:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,6 +41,6 @@ if ingredients_List:
         st.success('Your Smoothie is ordered,'+ name_on_order +"""!'""", icon="✅")
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
    
     
