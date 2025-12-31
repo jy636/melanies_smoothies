8a57253f9f6d94d5b0365a364d50a740f721ba4e
jy636 <jyl2003@med.cornell.edu>
2025-12-31 16:48:08 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 cnx=st.connection("snowflake")
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
