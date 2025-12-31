87bb9c81797018a8c5b4a08853aaf8c35d0e4373
jy636 <jyl2003@med.cornell.edu>
2025-12-31 17:27:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,11 @@ st.write('The name on your smoothie will be:',name_on_order)
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('search_on'))
-st.dataframe(data=my_dataframe,use_container_width =True)
+#st.dataframe(data=my_dataframe,use_container_width =True)
+#st.stop
+#convey snowpark dataframe to pandas dataframe, so we can use LOC function
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_dt)
 st.stop
 ingredients_List = st.multiselect('Choose up to 5 ingredients:',my_dataframe,max_selections=5)
 
